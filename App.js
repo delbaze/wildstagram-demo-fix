de8b5d6ceacd34e5dc9179968adddef81832f51f
delbaze <56109313+delbaze@users.noreply.github.com>
2023-07-04 15:41:55 +0200

    first commit

---
+++ App.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import CameraScreen from "./screens/CameraScreen";
+import FeedScreen from "./screens/FeedScreen";
+import ImagesScreen from "./screens/ImagesScreen";
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === "Camera") {
+              iconName = focused ? "camera" : "camera-outline";
+            } else if (route.name === "Images") {
+              iconName = focused ? "image" : "image-outline";
+            } else if (route.name === "Feed") {
+              iconName = focused ? "share-social" : "share-social-outline";
+            }
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: "blue",
+          tabBarInactiveTintColor: "gray",
+        })}
+      >
+        <Tab.Screen name="Camera" component={CameraScreen} options={{ unmountOnBlur: true }} />
+        <Tab.Screen name="Images" component={ImagesScreen} />
+        <Tab.Screen name="Feed" component={FeedScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
+
